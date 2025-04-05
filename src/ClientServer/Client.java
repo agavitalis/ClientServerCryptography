ed95dd262ab67b09a1cf8eeb17665fec9c5b9434
Vitalis <agavitalisogbonna@gmail.com>
2025-04-05 02:12:28 +0100

    affected prof corrections

--- src/ClientServer/Client.java
+++ src/ClientServer/Client.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.net.Socket;
 import java.security.*;
 import javax.crypto.*;
-import javax.crypto.spec.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
@@ -13,49 +12,46 @@ public class Client {
             
             // Key exchange
             KeyPair clientKP = Utils.generateDHKeyPair();
-            SecretKey sharedSecret = performKeyExchange(socket, clientKP);
-            
+            byte[] sharedBytes = performKeyExchange(socket, clientKP);
+
+            SecretKey encryptionKey = Utils.deriveAESKey(sharedBytes, 0);
+            SecretKey hmacKey = Utils.deriveHMACKey(sharedBytes, 16);
+
             // Mutual authentication
-            if (!performMutualAuthentication(socket, sharedSecret)) {
+            if (!performMutualAuthentication(socket, hmacKey)) {
                 System.out.println("Authentication failed");
                 return;
             }
-            
+
             // Secure data transmission
-            sendStudentData(socket, sharedSecret);
+            sendStudentData(socket, encryptionKey);
         }
     }
 
-    private static SecretKey performKeyExchange(Socket socket, KeyPair clientKP) throws Exception {
-        // Send client public key
+    private static byte[] performKeyExchange(Socket socket, KeyPair clientKP) throws Exception {
         Utils.sendPublicKey(socket, clientKP.getPublic());
-        
-        // Receive server public key
-        PublicKey serverPublicKey = Utils.receivePublicKey(socket);
-        
-        // Generate shared secret
-        return Utils.generateSharedSecret(clientKP.getPrivate(), serverPublicKey);
+        PublicKey serverPub = Utils.receivePublicKey(socket);
+        return Utils.generateSharedBytes(clientKP.getPrivate(), serverPub);
     }
 
-    private static boolean performMutualAuthentication(Socket socket, SecretKey key) throws Exception {
-        // Server authentication
-        String serverChallenge = Utils.receiveEncryptedMessage(socket, key);
-        Utils.sendEncryptedMessage(socket, serverChallenge, key);
-        
-        // Client authentication
+    private static boolean performMutualAuthentication(Socket socket, SecretKey hmacKey) throws Exception {
+        String serverChallenge = Utils.receiveMessage(socket);
+        byte[] hmac = Utils.generateHMAC(serverChallenge, hmacKey);
+        Utils.sendHMAC(socket, hmac);
+
         String clientChallenge = Utils.generateChallenge();
-        Utils.sendEncryptedMessage(socket, clientChallenge, key);
-        String response = Utils.receiveEncryptedMessage(socket, key);
-        
-        return Utils.verifyChallenge(clientChallenge, response);
+        Utils.sendMessage(socket, clientChallenge);
+        byte[] responseHmac = Utils.receiveHMAC(socket);
+
+        return MessageDigest.isEqual(hmac, Utils.generateHMAC(serverChallenge, hmacKey)) &&
+               MessageDigest.isEqual(responseHmac, Utils.generateHMAC(clientChallenge, hmacKey));
     }
 
-    private static void sendStudentData(Socket socket, SecretKey key) throws Exception {
+    private static void sendStudentData(Socket socket, SecretKey encryptionKey) throws Exception {
         Student student = new Student("Vitalis", "Ogbonna", 27);
-        SealedObject sealedStudent = Utils.sealObject(student, key);
-        
+        SealedObject sealed = Utils.sealObject(student, encryptionKey);
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        oos.writeObject(sealedStudent);
+        oos.writeObject(sealed);
         System.out.println("Sent encrypted Student object");
     }
 }
--- src/ClientServer/Utils.java
+++ src/ClientServer/Utils.java
@@ -5,41 +5,72 @@ import javax.crypto.spec.*;
 import java.io.*;
 import java.net.Socket;
 import java.security.*;
-import java.security.spec.*;
-import java.util.Base64;
 import java.util.Random;
 
 public class Utils {
-    // Cryptographic constants
     private static final String DH_ALGORITHM = "DH";
     private static final String AES_ALGORITHM = "AES";
+    private static final String HMAC_ALGORITHM = "HmacSHA256";
     private static final String CIPHER_TRANSFORMATION = "AES/GCM/NoPadding";
     private static final int GCM_TAG_LENGTH = 128;
     private static final int AES_KEY_SIZE = 16;
-    private static final int DH_KEY_SIZE = 2048;
 
-    //================ DH Key Exchange Methods ================//
+    // Key exchange
     public static KeyPair generateDHKeyPair() throws Exception {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(DH_ALGORITHM);
-        keyGen.initialize(DH_KEY_SIZE);
-        return keyGen.generateKeyPair();
+        KeyPairGenerator gen = KeyPairGenerator.getInstance(DH_ALGORITHM);
+        gen.initialize(2048);
+        return gen.generateKeyPair();
     }
 
-    public static KeyPair generateDHKeyPair(DHParameterSpec params) throws Exception {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(DH_ALGORITHM);
-        keyGen.initialize(params);
-        return keyGen.generateKeyPair();
+    public static byte[] generateSharedBytes(PrivateKey privateKey, PublicKey publicKey) throws Exception {
+        KeyAgreement agreement = KeyAgreement.getInstance(DH_ALGORITHM);
+        agreement.init(privateKey);
+        agreement.doPhase(publicKey, true);
+        return agreement.generateSecret();
     }
 
-    public static SecretKey generateSharedSecret(PrivateKey privateKey, PublicKey publicKey) throws Exception {
-        KeyAgreement keyAgreement = KeyAgreement.getInstance(DH_ALGORITHM);
-        keyAgreement.init(privateKey);
-        keyAgreement.doPhase(publicKey, true);
-        byte[] sharedSecret = keyAgreement.generateSecret();
-        return new SecretKeySpec(sharedSecret, 0, AES_KEY_SIZE, AES_ALGORITHM);
+    public static SecretKey deriveAESKey(byte[] sharedSecret, int offset) {
+        return new SecretKeySpec(sharedSecret, offset, AES_KEY_SIZE, AES_ALGORITHM);
     }
 
-    //================ Secure Communication Methods ================//
+    public static SecretKey deriveHMACKey(byte[] sharedSecret, int offset) {
+        return new SecretKeySpec(sharedSecret, offset, AES_KEY_SIZE, HMAC_ALGORITHM);
+    }
+
+    // Message passing
+    public static void sendMessage(Socket socket, String msg) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        oos.writeObject(msg);
+        oos.flush();
+    }
+
+    public static String receiveMessage(Socket socket) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        return (String) ois.readObject();
+    }
+
+    public static void sendHMAC(Socket socket, byte[] hmac) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        oos.writeObject(hmac);
+        oos.flush();
+    }
+
+    public static byte[] receiveHMAC(Socket socket) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        return (byte[]) ois.readObject();
+    }
+
+    public static byte[] generateHMAC(String message, SecretKey key) throws Exception {
+        Mac mac = Mac.getInstance(HMAC_ALGORITHM);
+        mac.init(key);
+        return mac.doFinal(message.getBytes("UTF-8"));
+    }
+
+    public static String generateChallenge() {
+        return Long.toString(System.currentTimeMillis()) + "-" + new Random().nextInt(999999);
+    }
+
+    // Sealing and Unsealing
     public static SealedObject sealObject(Serializable obj, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
         cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(GCM_TAG_LENGTH, new byte[12]));
@@ -52,16 +83,7 @@ public class Utils {
         return sealedObj.getObject(cipher);
     }
 
-    //================ Authentication Methods ================//
-    public static String generateChallenge() {
-        return Long.toString(System.currentTimeMillis()) + "-" + new Random().nextInt(100000);
-    }
-
-    public static boolean verifyChallenge(String challenge, String response) {
-        return challenge.equals(response);
-    }
-
-    //================ Network I/O Methods ================//
+    // Public key I/O
     public static void sendPublicKey(Socket socket, PublicKey publicKey) throws IOException {
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.writeObject(publicKey);
@@ -71,32 +93,5 @@ public class Utils {
     public static PublicKey receivePublicKey(Socket socket) throws Exception {
         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
         return (PublicKey) ois.readObject();
-    }
-
-    public static void sendEncryptedMessage(Socket socket, String message, SecretKey key) throws Exception {
-        byte[] encrypted = encryptString(message, key);
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        oos.writeObject(encrypted);
-        oos.flush();
-    }
-
-    public static String receiveEncryptedMessage(Socket socket, SecretKey key) throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        byte[] encrypted = (byte[]) ois.readObject();
-        return decryptString(encrypted, key);
-    }
-
-    //================ Core Crypto Methods ================//
-    private static byte[] encryptString(String plaintext, SecretKey key) throws Exception {
-        Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
-        cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(GCM_TAG_LENGTH, new byte[12]));
-        return cipher.doFinal(plaintext.getBytes("UTF-8"));
-    }
-
-    private static String decryptString(byte[] ciphertext, SecretKey key) throws Exception {
-        Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
-        cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(GCM_TAG_LENGTH, new byte[12]));
-        byte[] decrypted = cipher.doFinal(ciphertext);
-        return new String(decrypted, "UTF-8");
     }
 }
